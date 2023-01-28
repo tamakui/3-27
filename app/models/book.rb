f1e3549fb88cfb7a30d71addc6058de5004f48ce
EC2 Default User <ec2-user@ip-172-31-44-91.ap-northeast-1.compute.internal>
2023-01-28 16:20:02 +0000

    top

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,7 @@
 class Book < ApplicationRecord
     has_one_attached :profile_image
     belongs_to :user
+    has_many :favorites, dependent: :destroy
+    has_many :post_comments, dependent: :destroy
     
 end
