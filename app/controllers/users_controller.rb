ea24c1cbebd7af275948d3f99a975d49a77eb6b6
EC2 Default User <ec2-user@ip-172-31-44-91.ap-northeast-1.compute.internal>
2023-02-18 14:19:59 +0000

    [dele]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  #before_action :authenticate_user!
-  #before_action :ensure_correct_user, only:[:edit, :show]
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only:[:edit, :show]
 
 
   def show
--- app/models/book.rb
+++
@@ -1,8 +0,0 @@
-class Book < ApplicationRecord
-    has_one_attached :profile_image
-    belongs_to :user
-
-  validates :title, presence: true
-  validates :body, presence: true, length: { maximum: 200 }
-
-end
